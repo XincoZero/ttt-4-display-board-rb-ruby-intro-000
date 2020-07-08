1ca6ca7993318c909d3d9f51b05b9c358edab054
Hector Alvarez <h-alvarez@wiu.edu>
2020-07-08 04:12:20 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,5 @@
 def display_board(board)
+  
   puts " #{board[0]}  | #{board[1]}  | #{board[2]}  "
   puts "-----------"
   puts " #{board[3]}  | #{board[4]}  | #{board[5]}  "
@@ -6,4 +7,4 @@ def display_board(board)
   puts " #{board[6]}  | #{board[7]}  | #{board[8]}  "
 end 
 
-board= [" ", " ", " ", " ", " ", " ", " " ," ", " "]
+board = [" ", " ", " ", " ", " ", " ", " " ," ", " "]
